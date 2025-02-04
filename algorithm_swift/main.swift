32894b60dc8c5852cf6f97ebe511543cbd2e301b
심근웅 <geunwoong.sim@gmail.com>
2025-02-04 11:49:55 +0900

    add pro43165, code BFS

--- algorithm_swift/main.swift
+++ algorithm_swift/main.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 //print(solution())
-print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
-
-// input 데이터 생성
-func inputDataCreate() -> [Int] {
-    let MAX: Int = 10
-    var inputData: [Int] = []
-    for num in 1...MAX {
-        inputData.append(num)
-    }
-//    print(inputData)
-    return inputData
-}
+print(solution([1,2,3,4]  ,  4))
 
--- algorithm_swift/programmers/pro43165.swift
+++ algorithm_swift/programmers/pro43165.swift
@@ -13,6 +13,30 @@ func dfs(index:Int, sum: Int, result: Int, numbers: [Int], target: Int) -> Int{
     return b
 }
 
+func bfs(_ numbers:[Int], _ target:Int) -> Int {
+    var result: Int = 0
+    var queue: [(Int, Int)] = [(0, numbers[0]),(0, -numbers[0])] // $0: index, $1: sum
+    var index: Int = 0 // queue에 접근할 변수
+    while index < queue.count {
+        let element = queue[index]
+        index += 1
+        let idx = element.0 + 1
+        let sum1 = element.1 + numbers[idx]
+        let sum2 = element.1 - numbers[idx]
+        
+        if idx == numbers.count - 1 { // 더이상 enqueue하면 안됨
+            if sum1 == target { result += 1 } // 타겟넘버를 만들 수 있음
+            if sum2 == target { result += 1 } // 타겟넘버를 만들 수 있음
+        }else { // 값을 구해서 새롭게 enqueue
+            queue.append((idx, sum1))
+            queue.append((idx, sum2))
+        }
+    }
+    return result
+}
+
+
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    return dfs(index: 0, sum: 0, result: 0, numbers: numbers, target: target)
+    return bfs(numbers, target)
+//    return dfs(index: 0, sum: 0, result: 0, numbers: numbers, target: target)
 }
